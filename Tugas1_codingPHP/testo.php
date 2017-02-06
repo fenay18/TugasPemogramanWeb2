08c40deaaffc5d4c8bea7d6264933332bde7d225
Fheny Anggraeni Rahmat <fennyanggrae18@gmail.com>
2017-02-06 11:57:54 +0700

    Update testo.php

--- Tugas1_codingPHP/testo.php
+++ Tugas1_codingPHP/testo.php
@@ -3,13 +3,13 @@
   <body>
   <?php
 
-$NIM 		: "14.111.033";
-$NAMA 		: "FHENY ANGGRAENI RAHMAT";
-$JURUSAN 	: "TEKNIK INFORMATIKA";
+$NIM ="14.111.033";
+$NAMA ="FHENY ANGGRAENI RAHMAT";
+$JURUSAN ="TEKNIK INFORMATIKA";
 
-echo "NIM		: " . $NIM . "<br>";
-echo "NAMA		: " . $NAMA . "<br>";
-echo "JURUSAN	: " . $JURUSAN . "<br>";
+echo "NIM : " . $NIM . "<br>";
+echo "NAMA : " . $NAMA . "<br>";
+echo "JURUSAN : " . $JURUSAN . "<br>";
 
 ?>
 </html>
