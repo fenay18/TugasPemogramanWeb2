8904e36013f1be5f66a2bf6fe80d6e8735f74860
Fheny Anggraeni Rahmat <fennyanggrae18@gmail.com>
2017-02-06 11:51:40 +0700

    Add files via upload

---
+++ Tugas1_codingPHP/jurusan.php
@@ -0,0 +1,9 @@
+<?php
+
+$list_jurusan = array ("TI", "TIF", "DKV");
+echo "Daftar Jurusan<br>";
+echo "$list_jurusan[0]<br>";
+echo "$list_jurusan[1]<br>";
+echo "$list_jurusan[2]<br>";
+
+?>
---
+++ Tugas1_codingPHP/testo.php
@@ -0,0 +1,12 @@
+<?php
+
+$NIM 		: "14.111.033";
+$NAMA 		: "FHENY ANGGRAENI RAHMAT";
+$JURUSAN 	: "TEKNIK INFORMATIKA";
+
+echo "NIM		: " . $NIM . "<br>";
+echo "NAMA		: " . $NAMA . "<br>";
+echo "JURUSAN	: " . $JURUSAN . "<br>";
+
+?>
+
